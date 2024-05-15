7f97440743c714bf73985308c61a51487d0b87ea
Ravi Theja <ravitheja990@gmail.com>
2024-05-15 18:23:49 +0000

    Homepage layout initial setup

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-// src/App.js
-
-import React, { useState } from 'react';
+import logo from './logo.svg';
+// import './static';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Message from './Message';
+import HomePage from './components/HomePage';
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form Data Submitted: ', formData);
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Contact Form</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>Name:</label>
-            <input type="text" name="name" value={formData.name} onChange={handleChange} />
-          </div>
-          <div>
-            <label>Email:</label>
-            <input type="email" name="email" value={formData.email} onChange={handleChange} />
-          </div>
-          <div>
-            <label>Message:</label>
-            <textarea name="message" value={formData.message} onChange={handleChange} />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </header>
-    </div>
-  );
+  // return <div><Message></Message></div>;
+  return <div><HomePage></HomePage></div>;
+  // return (
+  //   return <div><Message/></div>
+  // );
 }
 
 export default App;
