170213bd16ac90078def32e062d96d0e8e166523
Ravi Theja <ravitheja990@gmail.com>
2024-06-03 17:45:33 +0000

    UI version 1 changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,85 @@
 import React, { useState } from 'react';
 import FileStructure from './FileStructure';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function HomePage() {
-    const [showDiv, setShowDiv] = useState(false);
+    const [activeDiv, setActiveDiv] = useState(null);
 
-    const palleteIconClasses = ['glyphicon glyphicon-plus', 'glyphicon glyphicon-plus', 'glyphicon glyphicon-plus', 'glyphicon glyphicon-plus'];
+    const handleButtonClick = (type) => {
+        setActiveDiv(type);
+    };
 
-    const palatteIcon = (iconClass) => {
+    const palleteIconClasses = ['fas fa-plus', 'fa-regular fa-folder', 'fas fa-play', 'fa-solid fa-pause'];
+    const colors = ['#b02929ed', '#cac000', '#88d500', '#dba405'];
+
+    const palatteIcon = (iconClass, color) => {
         return (
             <div className='palatte-icon'>
-                <button type="button" className="btn btn-default btn-sm">
-                    <span className={iconClass}></span> Plus
-                </button>
+                <a className='palatte-icon d-flex justify-content-center align-items-center' href="#" style={{ textDecoration: 'none', paddingTop: '10px' }}>
+                    <span style={{ fontSize: '30px', color: color }} className={iconClass}></span>
+                </a>
                 <hr />
             </div>
         );
     }
 
     const divStyle = {
-        display: showDiv ? 'block' : 'none',
+        display: 'block',
         backgroundColor: '#f0f0f0',
-        padding: '20px',
-        marginTop: '20px',
+        // position: 'absolute',
+        top: '0',
+        left: '0',
+        right: '0',
+        bottom: '0',
+        padding: '420px',
+    };
+
+    const textContainerStyle = {
+        // padding: '10px',
+        top: '-100',
+        left: '0',
+        right: '0',
+        bottom: '0'
+    };
+
+    const renderContent = () => {
+        switch (activeDiv) {
+            case 'Status':
+                return <div style={divStyle}>
+                            <div style={textContainerStyle}>
+                            <div>
+                                <label>Job Path:</label>
+                                {/* <span> {jobPath}</span> */}
+                            </div>
+                            <div>
+                                <label>Job Status:</label>
+                                {/* <span> {jobStatus}</span> */}
+                            </div>
+                            <div>
+                                <label>Run Length:</label>
+                                {/* <span> {runLength}</span> */}
+                            </div>
+                        </div>
+                        </div>;
+            case 'Setup':
+                return <div style={divStyle}>Setup Content</div>;
+            case 'Namelists':
+                return <div style={divStyle}>Namelists Content</div>;
+            case 'Output':
+                return <div style={divStyle}>Output Content</div>;
+            case 'Plots':
+                return <div style={divStyle}>Plots Content</div>;
+            default:
+                return null;
+        }
     };
 
     return (
         <>
             <div>
                 <p className="top-text">Job</p>
-                <div className="parent">
+                <div className="parent" style={{ position: 'relative' }}>
                     <div className="palatte-area">
                         <FileStructure />
                     </div>
@@ -38,18 +88,18 @@ function HomePage() {
                             {
                                 palleteIconClasses.map((iconClass, index) => (
                                     <div key={index}>
-                                        {palatteIcon(iconClass)}
+                                        {palatteIcon(iconClass, colors[index])}
                                     </div>
                                 ))
                             }
                         </div>
-                        <div className='task'>
-                            
-                            <button>Status</button>
-                            <button>Setup</button>
-                            <button>Namelists</button>
-                            <button>Output</button>
-                            <button>Plots</button>
+                        <div className='task' style={{ position: 'relative' }}>
+                            <button onClick={() => handleButtonClick('Status')}>Status</button>
+                            <button onClick={() => handleButtonClick('Setup')}>Setup</button>
+                            <button onClick={() => handleButtonClick('Namelists')}>Namelists</button>
+                            <button onClick={() => handleButtonClick('Output')}>Output</button>
+                            <button onClick={() => handleButtonClick('Plots')}>Plots</button>
+                            {renderContent()}
                         </div>
                     </div>
                 </div>
